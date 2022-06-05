3a57a978430fe1485b31a1eb2cbd05cccfd12a7b
Charles Beynon <eToThePiIPower@gmail.com>
2022-06-04 21:07:44 -0400

    Add dark theme for `.code-figure` blocks
    Added a dark theme for the `.code-figure` blocks generate by the `{%
    include_code %}` Jekyll tag.  This required a few additional extractions
    and refactorings to support.
    
    * Updated the `code_marking` mixin to support colors as arguments. Set
      the default values to what were currently being used, so the original
      light-themed styles can work without change.
    * Tweaked the color ans size of the scrollbars so they can be more
      visible, and added a scrollbar-corner style. Code blocks added with
      `{% include_code %}` can be long enough that a vertical scroll may be
      necessary (see below).
    * Added a `max-height` to `.code-figure` blocks of `85vh`. This will
      ensure that longer code blocks will always fit onto a single screen,
      with a bit of breathing room.
    * Added an example of `{% include_code %}` to an example post. I'm using
      my `octopress_image_caption` gem as an example, because it's both long
      enough to serve as an example and has some relevant portions (such as
      HEREDOC strings) to call out special scenarios.

--- _drafts/example-1.md
+++ _drafts/example-1.md
@@ -23,3 +23,15 @@ pre-asides: # Add as many notices as you want, or delete this block
 
 This post is an example post. If you can see this message, the post was
 probably published by mistake.
+
+You can include code from external files using the `included_code` Jekyll tag (which is aliased to `render_code`). Using this tag, you can make use of features such as figure titles, download links,
+and highlighting selected lines.
+
+One consideration to keep in mind here is that HEREDOC-style strings are not escaped properly, which
+will corrupt your html document if they are used inside your included code. As in the highlighted
+portions below, you can comment out those sections to render the code block correctly.
+
+The standard stylesheet uses the same `xcode` theme the rest of the codeblocks use. As with other code blocks, I've enabled a `dark` style here as well, used in the same way by adding `{:.dark}` before the `include_code` tag.
+
+{:.dark}
+{% render_code example.rb title:"Example Ruby File" lang:ruby mark:37-46,50-59,63-72 url:test.rb %}
---
+++ code-examples/example.rb
@@ -0,0 +1,160 @@
+require 'octopress-image-caption-tag/version'
+require 'liquid'
+
+# Title: Simple Image tag for Jekyll
+# Authors: Charles Beynon https://eulerpi.io
+#          Brandon Mathis http://brandonmathis.com
+#          Felix Schäfer, Frederic Hemberger
+#
+
+module Octopress
+  module Tags
+    module ImageCaptionTag
+      module ImageCaptionFunctions
+        def parse_sizes(raw_size)
+          if /\s*(?<w>\d+(\.\d+)?%?(em)?)\s+(?<h>\d+(\.\d+)?%?(em)?)/ =~ raw_size
+            @width = w
+            @height = h
+          elsif @class.rstrip == 'right' || @class.rstrip == 'left'
+            @width = '33%'
+          else
+            @width = '100%'
+          end
+        end
+
+        def parse_title(raw_title)
+          if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ raw_title
+            @title  = title
+            @alt    = alt
+          else
+            /(?:"|')(?<titlealt>[^"']+)?(?:"|')/ =~ raw_title
+            @title = titlealt
+            @alt = titlealt
+          end
+        end
+
+        def absolute_sized_figure
+          # <<-EOS.gsub(/^ {10}/, '') # gsubm is to pretty up source by indenting
+          # <figure class='image-caption image-caption-absolute #{@class.rstrip}'>
+          #   <a class='image-popup' href='#{@img}'>
+          #     <img class='caption' src='#{@img}' width='#{@width}' height='#{@height}' title='#{@title}' alt='#{@alt}'>
+          #   </a>
+          #   <figcaption class='caption-text'>
+          #     #{@caption}
+          #   </figcaption>
+          # </figure>
+          # EOS
+        end
+
+        def em_sized_figure
+          # <<-EOS.gsub(/^ {10}/, '') # gsubm is to pretty up source by indenting
+          # <figure class='image-caption image-caption-absolute #{@class.rstrip}'>
+          #   <a class='image-popup' href='#{@img}'>
+          #     <img class='caption' src='#{@img}' style='width:#{@width};height:#{@height};' title='#{@title}' alt='#{@alt}'>
+          #   </a>
+          #   <figcaption class='caption-text'>
+          #     #{@caption}
+          #   </figcaption>
+          # </figure>
+          # EOS
+        end
+
+        def relative_sized_figure
+          # <<-EOS.gsub(/^ {10}/, '') # gsubm is to pretty up source by indenting
+          # <figure class='image-caption #{@class.rstrip}'>
+          #   <a class='image-popup' href='#{@img}'>
+          #     <img class='caption' src='#{@img}' width='100%' height='100%' title='#{@title}' alt='#{@alt}'>
+          #   </a>
+          #   <figcaption class='caption-text'>
+          #     #{@caption}
+          #   </figcaption>
+          # </figure>
+          # EOS
+        end
+      end
+
+      class Tag < Liquid::Tag
+        include ImageCaptionFunctions
+        @img = nil
+        @title = nil
+        @class = ''
+        @width = ''
+        @height = ''
+
+        def initialize(tag_name, markup, tokens)
+          if %r{(?<classname>\S.*\s+)?(?<protocol>https?://|/)(?<url>\S+)(?<sizes>\s+\d+(\.\d+)?%?(em)?\s+\d+(\.\d+)?%?(em)?)?(?<title>\s+.+)?} =~ markup
+            @class = classname || 'center'
+            @img = "#{protocol}#{url}"
+            @title = title.strip if title
+            parse_sizes(sizes)
+            parse_title(@title)
+          end
+          super
+        end
+
+        def render(context)
+          super
+          @caption = @title
+          if @img && @width[-1] == '%' # Relative width, so width goes on outer span
+            relative_sized_figure
+          elsif @img && @width[-2..-1] == 'em' # Absolute size in ems
+            em_sized_figure
+          elsif @img # Absolute width, so width goes on the img tag and text span gets sytle-width:@width-15;
+            absolute_sized_figure
+          else
+            'Error processing input, expected syntax: {% imgcap [class name(s)] /url/to/image [width height] [title [alt]] %}'
+          end
+        end
+      end
+
+      class Block < Liquid::Block
+        include ImageCaptionFunctions
+        @img = nil
+        @title = nil
+        @class = ''
+        @width = ''
+        @height = ''
+
+        def initialize(tag_name, markup, tokens)
+          if %r{(?<classname>\S.*\s+)?(?<protocol>https?://|/)(?<url>\S+)(?<sizes>\s+\d+(\.\d+)?%?(em)?\s+\d+(\.\d+)?%?(em)?)?(?<title>\s+.+)?} =~ markup
+            @class = classname || 'center'
+            @img = "#{protocol}#{url}"
+            @title = title.strip if title
+            parse_sizes(sizes)
+            parse_title(@title)
+          end
+          super
+        end
+
+        def render(context)
+          site = context.registers[:site]
+          converter = site.find_converter_instance(Jekyll::Converters::Markdown)
+          @caption = converter.convert(super).lstrip.rstrip
+          if @img && @width[-1] == '%' # Relative width, so width goes on outer span
+            relative_sized_figure
+          elsif @img && @width[-2..-1] == 'em'
+            em_sized_figure
+          elsif @img # Absolute width, so width goes on the img tag and text span gets sytle-width:@width-15;
+            absolute_sized_figure
+          else
+            'Error processing input, expected syntax: {% imgcaption [class name(s)] /url/to/image [width height] [title [alt]] %} Caption Text {% endimgcaption %}'
+          end
+        end
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('imgcap', Octopress::Tags::ImageCaptionTag::Tag)
+Liquid::Template.register_tag('imgcaption', Octopress::Tags::ImageCaptionTag::Block)
+
+if defined? Octopress::Docs
+  Octopress::Docs.add(
+    name:        'Octopress Image Caption Tag',
+    gem:         'octopress-image-caption-tag',
+    description: 'A tag to create images with pretty captions for Jekyll and Octopress blogs',
+    path:        File.expand_path(File.join(File.dirname(__FILE__), '../')),
+    source_url:  'https://github.com/eToThePiIPower/octopress-image-caption-tag',
+    version:     Octopress::Tags::ImageCaptionTag::VERSION
+  )
+end
